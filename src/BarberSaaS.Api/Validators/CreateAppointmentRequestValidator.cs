ea7f571f626726d12ce16b199d7af1d6d2b4940f
furkan-erhan <osasmortal@gmail.com>
2026-03-01 08:33:01 +0300

    feat: added update request validators for appointment and barber shop. #12

---
+++ src/BarberSaaS.Api/Validators/CreateAppointmentRequestValidator.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+using BarberSaaS.Api.DTOs;
+using System.Data;
+
+namespace BarberSaaS.Api.Validators;
+
+
+public class CreateAppointmentRequestValidator : AbstractValidator<CreateAppointmentDto>
+{
+    public CreateAppointmentRequestValidator()
+    {
+        RuleFor(x => x.BarberShopId).NotEmpty();
+        RuleFor(x => x.EmployeeId).NotEmpty();
+        RuleFor(x => x.CustomerId).NotEmpty();
+
+        RuleFor(x => x.StartTime)
+            .NotEmpty()
+            .GreaterThan(DateTime.UtcNow).WithMessage("Appointment must be in the future");
+
+        RuleFor(x => x.EndTime)
+            .NotEmpty()
+            .GreaterThan(x => x.StartTime).WithMessage("End time must be after start time");
+
+        RuleFor(x => x.Price)
+            .GreaterThanOrEqualTo(0).When(x => x.Price.HasValue);
+    }
+}
