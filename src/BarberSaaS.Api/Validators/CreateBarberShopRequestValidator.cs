75cec1cbb73367b72f7daaa9a2ee80d43d6dbbdd
furkan-erhan <osasmortal@gmail.com>
2026-03-01 08:21:23 +0300

    feat: installed validation packet and built validators for appointment and barber shop. #12

---
+++ src/BarberSaaS.Api/Validators/CreateBarberShopRequestValidator.cs
@@ -0,0 +1,20 @@
+using FluentValidation;
+using BarberSaaS.Api.DTOs;
+
+namespace BarberSaaS.Api.Validators;
+
+
+public class CreateBarberShopRequestValidator : AbstractValidator<CreateBarberShopDto>
+{
+    public CreateBarberShopRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Barber shop name is required")
+            .MaximumLength(100).WithMessage("Name can't exceed 100 characters");
+
+        RuleFor(x => x.Slug)
+            .NotEmpty().WithMessage("Slug is required")
+            .Matches(@"^[a-z0-9-]+$").WithMessage("Slug can only contain lowercase letters, numbers and hypens")
+            .MaximumLength(25).WithMessage("Slug name can't exceed 25 characters");
+    }
+}
